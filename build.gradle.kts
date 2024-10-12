5b7b8a9dab7180cd86a81a898c6ab577f1404f52
ajhek <adynhek@gmail.com>
2024-10-12 16:09:35 -0400

    bot can now respond to messages in channel

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
+    application
+    id("com.gradleup.shadow") version "8.3.1"
     id("java")
 }
 
+application.mainClass = "com.example.dndbot.Bot"
 group = "org.example"
-version = "1.0-SNAPSHOT"
+version = "1.0"
+
+val jdaVersion = "5.1.2"
 
 repositories {
     mavenCentral()
@@ -12,6 +17,15 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("net.dv8tion:JDA:$jdaVersion")
+    implementation("ch.qos.logback:logback-classic:1.5.6")
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.isIncremental = true
+
+    sourceCompatibility = "1.8"
 }
 
 tasks.test {
--- src/main/java/com/example/dndbot/Bot.java
+++ src/main/java/com/example/dndbot/Bot.java
@@ -1,4 +1,28 @@
 package com.example.dndbot;
 
-public class Bot {
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+public class Bot extends ListenerAdapter{
+    public static void main(String[] arguments) throws Exception {
+        JDA api = JDABuilder.createDefault(System.getenv("DISCORD_TOKEN")).enableIntents(GatewayIntent.MESSAGE_CONTENT).build();
+        api.addEventListener(new Bot());
+    }
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+        if (event.getAuthor().isBot()) return;
+        // We don't want to respond to other bot accounts, including ourself
+        Message message = event.getMessage();
+        String content = message.getContentRaw();
+        // getContentRaw() is an atomic getter
+        // getContentDisplay() is a lazy getter which modifies the content for e.g. console view (strip discord formatting)
+        if (content.equals("!ping"))
+        {
+            event.getChannel().sendMessage("pong!").queue();
+        }
+    }
 }
