0babdad242d0def7b5aa0f1b4837b8aa6c33e1db
ajhek <adynhek@gmail.com>
2024-10-15 00:25:09 -0400

    Continued work on commands

--- src/main/java/com/example/dndbot/SlashCommands.java
+++ src/main/java/com/example/dndbot/SlashCommands.java
@@ -1,57 +1,19 @@
 package com.example.dndbot;
 
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
-import net.dv8tion.jda.api.requests.GatewayIntent;
-import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 
-
-import javax.security.auth.login.LoginException;
 import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.Random;
 
-public class Bot extends ListenerAdapter{
-    public static void main(String[] arguments) throws LoginException {
-        JDA api = JDABuilder.createDefault(System.getenv("DISCORD_TOKEN")).enableIntents(GatewayIntent.MESSAGE_CONTENT)
-                .build();
-        //CommandListUpdateAction commands = api.updateCommands();
-        api.updateCommands();
-
-        //api.updateCommands().addCommands();
-
-        api.addEventListener(new SlashCommands());
-        //commands.queue();
-        System.out.println("init");
-    }
-
-    /*public void onMessageReceived(MessageReceivedEvent event)
-    {
-        System.out.println("Text command");
-        if (event.getAuthor().isBot()) return;
-        // We don't want to respond to other bot accounts, including ourselves
-        Message message = event.getMessage();
-        String content = message.getContentRaw();
-        // getContentRaw() is an atomic getter
-        // getContentDisplay() is a lazy getter which modifies the content for e.g. console view (strip discord formatting)
-        if (content.equals("!ping"))
-        {
-            event.getChannel().sendMessage("pong!").queue();
-        }
-    }*/
-
-
-    /*public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
-        event.deferReply().queue();
+public class SlashCommands extends ListenerAdapter {
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) throws IndexOutOfBoundsException{
         System.out.println("Command Received");
         if (event.getName().equals("roll")) {
             Long userId = event.getUser().getIdLong();
@@ -70,6 +32,7 @@ public class Bot extends ListenerAdapter{
             event.reply(rollmsg).queue();
         }
         if (event.getName().equals("addchar")) {
+            event.deferReply().queue();
             Long userId = event.getUser().getIdLong();
             String charName = event.getOption("name").getAsString();
             Dictionary<String, Integer> stats = new Hashtable<>();
@@ -81,7 +44,33 @@ public class Bot extends ListenerAdapter{
 
             event.reply("Character " + charName + " added!").queue();
         }
-    }*/
+    }
 
+    private static final Command.Choice[] choiceArray = {
+            new Command.Choice("ranged", "Attack with a ranged weapon"),
+            new Command.Choice("melee", "Attack with a melee weapon"),
+            new Command.Choice("CQC", "Attack with your fists"),
+            new Command.Choice("dodge", "Dodge an incoming attack"),
+            new Command.Choice("block", "Withstand an incoming attack"),
+            new Command.Choice("throwables", "Throw an object at a target"),
+            new Command.Choice("perception", "Spot an incoming attack or hidden enemy"),
+            new Command.Choice("bigbrain", "Determine if an action will have an intended effect"),
+            new Command.Choice("speech", "Convince a target of something"),
+            new Command.Choice("stealth", "Hide yourself or sneak up on a target")
+    };
 
+    public void onGuildReady(GuildReadyEvent event) {
+        event.getGuild().updateCommands().addCommands(
+                Commands.slash("roll", "Roll to perform an action")
+                        .addOptions(new OptionData(OptionType.STRING, "action", "Pick your action", true)
+                                .addChoices(choiceArray))
+                        .addOptions(new OptionData(OptionType.INTEGER, "modifier", "Add an additional modifier to your roll", false)),
+                Commands.slash("addchar", "Add a character")
+                        .addOptions(new OptionData(OptionType.STRING, "name", "Name your character", true))
+        ).queue();
+    }
+    protected Dictionary<Long, Dictionary<String, Integer>> characters = new Hashtable<>();
+    protected Dictionary<Long, String> charNames = new Hashtable<>();
+    protected String[] npcs = {};
+    protected Random rand = new Random();
 }
